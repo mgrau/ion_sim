17b25413160dfbf0b7e8978eddebbb2ed410fc3c
Matt Grau <matt.grau@gmail.com>
2021-03-09 14:36:31 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(name='ion_sim',
+      version='0.1.0',
+      description='solve for ion motion in arbitrary potentials',
+      url='https://github.com/mgrau/ion_sim',
+      author='Matt Grau',
+      author_email='matt.grau@gmail.com',
+      python_requires='>=3.6',
+      install_requires=[
+              'numpy',
+              'scipy',
+              'pint'
+      ],
+      packages=find_packages())
